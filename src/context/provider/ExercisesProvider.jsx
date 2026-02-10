0af5d65a4d85f5c254d0a3209f4ee882f21b35e3
komodu <dagliwill@gmail.com>
2026-02-10 18:07:11 +0800

    added customHook for ExerciseContext

--- src/context/provider/ExercisesProvider.jsx
+++ src/context/provider/ExercisesProvider.jsx
@@ -1,35 +1,9 @@
-import { useReducer, useEffect, useState } from "react";
-import { exercisesReducer } from "../../reducer/exerciseReducer";
-import { ExercisesContext } from "../Context";
+import { useExercises } from "../../hooks/useExercises";
 import LoaderSVG from "../../assets/img/loader.svg";
-const initialState = {
-  exercises: [],
-};
+import { ExercisesContext } from "../Context";
 const ExercisesProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(exercisesReducer, initialState);
-  const [exLoading, setExLoading] = useState(true);
-  useEffect(() => {
-    const fetchUserExercises = async () => {
-      try {
-        const response = await fetch("/api/exercise/");
-        if (!response.ok)
-          return new Error("Error in fetching Exercises in Context");
-        const data = await response.json();
+  const { exLoading, state, dispatch } = useExercises();
 
-        dispatch({ type: "SET_EXERCISES", payload: data });
-      } catch (error) {
-        console.error("Error in Exercise Provider: ", error);
-      }
-    };
-    // Set loader for 2secs after fetch
-    fetchUserExercises();
-    const timer = setTimeout(() => {
-      setExLoading(false);
-    }, 2000);
-    return () => clearTimeout(timer);
-  }, []);
-
-  console.log("ExercisesProvider: ", state.exercises);
   if (exLoading) {
     return (
       <div className="workouts-loading">
---
+++ src/hooks/useExercises.jsx
@@ -0,0 +1,33 @@
+import { exercisesReducer } from "../reducer/exerciseReducer";
+import { useReducer, useState, useEffect } from "react";
+const initialState = {
+  exercise: [],
+};
+export const useExercises = () => {
+  const [state, dispatch] = useReducer(exercisesReducer, initialState);
+  const [exLoading, setExLoading] = useState(true);
+  useEffect(() => {
+    const fetchUserExercises = async () => {
+      try {
+        const response = await fetch("/api/exercise/");
+        if (!response.ok)
+          return new Error("Error in fetching Exercises in Context");
+        const data = await response.json();
+
+        dispatch({ type: "SET_EXERCISES", payload: data });
+      } catch (error) {
+        console.error("Error in Exercise Provider: ", error);
+      }
+    };
+    // Set loader for 2secs after fetch
+    fetchUserExercises();
+    const timer = setTimeout(() => {
+      setExLoading(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  console.log("ExercisesProvider: ", state.exercises);
+
+  return { exLoading, state, dispatch };
+};
